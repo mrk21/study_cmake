ebb2c9be0cf20b626729375325c445499fd869a2
Yuichi Murata <mrk21info+github@gmail.com>
2014-11-20 15:49:32 +0900

    Added the core/set_unset.

---
+++ core/set_unset.cmake
@@ -0,0 +1,19 @@
+#!/usr/bin/env cmake -P
+cmake_minimum_required(VERSION 3.0)
+
+# The PARENT_SCOPE option of set() and unset() commands make it possible to edit variables of the parent context.
+# see: http://www.cmake.org/cmake/help/v3.0/command/set.html
+
+function(set_value output_variable value)
+  set(${output_variable} ${value} PARENT_SCOPE)
+endfunction()
+
+function(unset_value output_variable)
+  unset(${output_variable} PARENT_SCOPE)
+endfunction()
+
+set_value(value 1)
+message("value: ${value}")
+
+unset_value(value)
+message("value: ${value}")
