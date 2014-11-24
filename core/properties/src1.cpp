bb715220398b036f8536fac4d58abd1dd3922672
Yuichi Murata <mrk21info+github@gmail.com>
2014-11-24 17:09:44 +0900

    Added the core/properties.

---
+++ core/properties/src1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#ifndef VALUE
+#define VALUE "normal_value"
+#endif
+
+int main() {
+    std::cout << "src1.cpp: " << VALUE << std::endl;
+    return 0;
+}
---
+++ core/properties/src2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#ifndef VALUE
+#define VALUE "normal_value"
+#endif
+
+int main() {
+    std::cout << "src2.cpp: " << VALUE << std::endl;
+    return 0;
+}
