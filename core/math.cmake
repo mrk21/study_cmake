65140c633bda4f9dc432ca292ccc682ecf7c1d08
Yuichi Murata <mrk21info+github@gmail.com>
2014-12-10 00:13:45 +0900

    Updated the core/math.

--- core/math.cmake
+++ core/math.cmake
@@ -1,5 +1,17 @@
 #!/usr/bin/env cmake -P
 cmake_minimum_required(VERSION 3.0.2)
 
-math(EXPR result "1+1")
-message("result: ${result}")
+function(verify_math expr)
+  math(EXPR result "${expr}")
+  message("${expr} = ${result}")
+endfunction()
+
+verify_math("1 + 1")
+verify_math("1 - 5")
+verify_math("2 * 3")
+verify_math("2 * 3 + (2 + 4)")
+verify_math("1 << 3")
+verify_math("8 >> 2")
+verify_math("12 % 10")
+verify_math("8 | 15")
+verify_math("8 & 15")
