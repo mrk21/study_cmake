8b2b6a1c72b8f723ee7cab0129c9d5493afb30bf
Yuichi Murata <mrk21info+github@gmail.com>
2014-12-14 21:39:42 +0900

    Updated the core/generator_expressions.

--- core/generator_expressions.cmake
+++ core/generator_expressions.cmake
@@ -3,6 +3,7 @@ cmake_minimum_required(VERSION 3.0.2)
 
 include(${CMAKE_CURRENT_LIST_DIR}/../_execute.cmake)
 execute_cmake()
+set(ENV{VERBOSE} 1)
 execute(make)
 execute(./src1_bin)
 execute(./src2_bin)
