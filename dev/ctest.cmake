9e10dd3364ed607d8e6553024c79a681b6fc0df8
Yuichi Murata <mrk21info+github@gmail.com>
2014-12-17 00:32:28 +0900

    Updated the dev/ctest.

--- dev/ctest.cmake
+++ dev/ctest.cmake
@@ -2,6 +2,7 @@
 cmake_minimum_required(VERSION 3.0.2)
 
 include(${CMAKE_CURRENT_LIST_DIR}/../_execute.cmake)
+cmake_host_system_information(RESULT core_size QUERY NUMBER_OF_LOGICAL_CORES)
 execute_cmake()
-execute(make)
-execute(make test ARGS="-V")
+execute(make -j${core_size})
+execute(make test "ARGS=-V -C Debug -j ${core_size}")
--- dev/ctest/test.cpp
+++ dev/ctest/test.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
+#include <thread>
+#include <cstdlib>
 #include <boost/filesystem.hpp>
 
 int main(int argc, char * argv[]) {
+    std::cout << "### test.cpp" << std::endl;
+    
     std::vector<std::string> args(argv, argv + argc);
-    std::cout << "######### test" << std::endl;
     for (auto && v: args) {
-        std::cout << "======= " << v << std::endl;
+        std::cout << "@@ args: " << v << std::endl;
     }
-    std::cout << boost::filesystem::current_path() << std::endl;
+    
+    std::cout << "@@ working_dir: " << boost::filesystem::current_path() << std::endl;
+    
+    std::cout << "@@ ${VALUE1}: ";
+    if (auto && env = std::getenv("VALUE1")) std::cout << env;
+    std::cout << std::endl;
+    
+    std::cout << "@@ ${VALUE2}: ";
+    if (auto && env = std::getenv("VALUE2")) std::cout << env;
+    std::cout << std::endl;
+    
+    std::cout << "@@ sleep..." << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds{2});
+    std::cout << "@@ finish" << std::endl;
+    
     return 0;
 }
