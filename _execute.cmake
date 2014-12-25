27e7b62494d14e404df63b5299aac0b06aa50885
Yuichi Murata <mrk21info+github@gmail.com>
2014-12-26 03:07:06 +0900

    Updated the core/convenience_commands.

--- _execute.cmake
+++ _execute.cmake
@@ -1,13 +1,39 @@
-function(execute)
-  get_filename_component(basepath ${CMAKE_CURRENT_LIST_FILE} DIRECTORY)
+cmake_minimum_required(VERSION 3.0.2)
+
+function(get_root_dir result)
   get_filename_component(name ${CMAKE_CURRENT_LIST_FILE} NAME_WE)
-  set(working_dir ${basepath}/${name}/gen)
+  set(${result} ${CMAKE_CURRENT_LIST_DIR}/${name} PARENT_SCOPE)
+endfunction()
+
+function(get_working_dir result)
+  get_root_dir(root_dir)
+  set(${result} ${root_dir}/gen PARENT_SCOPE)
+endfunction()
+
+function(init_working_dir)
+  set(args ${ARGV} false)
+  list(GET args 0 is_force)
+  unset(args)
   
-  if(NOT DEFINED IS_FIRST_EXECUTION)
+  if(is_force)
+    expire_working_dir()
+  endif()
+  
+  if(NOT DEFINED WORKING_DIR_INITIALIZED)
+    get_working_dir(working_dir)
     file(REMOVE_RECURSE ${working_dir})
     file(MAKE_DIRECTORY ${working_dir})
-    set(IS_FIRST_EXECUTION true CACHE INTERNAL "")
+    set(WORKING_DIR_INITIALIZED true CACHE INTERNAL "")
   endif()
+endfunction()
+
+function(expire_working_dir)
+  unset(WORKING_DIR_INITIALIZED CACHE)
+endfunction()
+
+function(execute)
+  get_working_dir(working_dir)
+  init_working_dir()
   
   string(REPLACE ";" " " commandline "${ARGV}")
   message("")
@@ -31,4 +57,43 @@ endfunction()
 
 function(execute_cmake)
   execute(${CMAKE_COMMAND} ${ARGV} ..)
+endfunction()
+
+function(make_cmakelists current_dir code)
+  get_root_dir(root_dir)
+  get_working_dir(working_dir)
+  
+  if(current_dir STREQUAL .)
+    set(current_dir ${root_dir})
+  else()
+    set(current_dir ${root_dir}/${current_dir})
+  endif()
+  
+  string(CONFIGURE "${code}" code @ONLY)
+  configure_file(${current_dir}/CMakeLists.txt.in ${current_dir}/CMakeLists.txt)
+endfunction()
+
+function(scope block)
+  get_root_dir(root_dir)
+  get_working_dir(working_dir)
+  
+  string(CONFIGURE "
+    macro(scope_impl)
+      ${block}
+    endmacro()
+  " block @ONLY)
+  
+  set(temp_path ${working_dir}/.temp.cmake)
+  file(WRITE ${temp_path} "${block}")
+  include(${temp_path})
+  file(REMOVE ${temp_path})
+  
+  unset(root_dir)
+  unset(working_dir)
+  unset(block)
+  unset(temp_path)
+  
+  init_working_dir()
+  scope_impl()
+  expire_working_dir()
 endfunction()
--- core/convenience_commands.cmake
+++ core/convenience_commands.cmake
@@ -1,25 +1,198 @@
 #!/usr/bin/env cmake -P
 cmake_minimum_required(VERSION 3.0.2)
 
+include(${CMAKE_CURRENT_LIST_DIR}/../_execute.cmake)
+
+message([[## file()]])
+message([[### WRITE]])
+scope([=[
+  file(WRITE @working_dir@/hoge.txt a b c) #[[
+    $ cat hoge.txt
+    abc
+  ]]
+  file(READ @working_dir@/hoge.txt content)
+  message("content: ${content}")
+  
+  file(APPEND @working_dir@/hoge.txt a b c) #[[
+    $ cat hoge.txt
+    abcabc
+  ]]
+  file(READ @working_dir@/hoge.txt content)
+  message("content: ${content}")
+]=])
+message("")
+
+message([[### READ]])
+scope([=[
+  file(WRITE @working_dir@/hoge.txt a b c)
+  
+  file(READ @working_dir@/hoge.txt content)
+  message("content: ${content}") # abc
+  
+  file(READ @working_dir@/hoge.txt content HEX)
+  message("content: ${content}") # 616263
+  
+  file(READ @working_dir@/hoge.txt content LIMIT 2 OFFSET 1)
+  message("content: ${content}") # bc
+]=])
+message("")
+
+message([[### STRINGS]])
+scope([[
+  file(STRINGS @root_dir@/test1.txt var)
+  message("var: ${var}")
+]])
+message("")
+
+message([[### GENERATE]])
+scope([[
+  make_cmakelists(. [=[
+    file(GENERATE
+      OUTPUT @working_dir@/test1.txt
+      CONTENT "ddd"
+      CONDITION $<AND:1,1>
+    )
+  ]=])
+  execute_cmake()
+  execute(cat test1.txt)
+  message("")
+]])
+message("")
+
+message([[### GLOB・GLOB_RECURSE]])
+scope([=[
+  file(GLOB files @root_dir@/*.txt)
+  message("files: ${files}")
+  
+  file(GLOB files @root_dir@/test1*.txt)
+  message("files: ${files}")
+  
+  file(GLOB files @root_dir@/[t-z]*.txt)
+  message("files: ${files}")
+  
+  file(GLOB files @root_dir@/*.gitignore)
+  message("files: ${files}")
+]=])
+message("")
+
+message([[### MD5]])
+scope([[
+  file(MD5 @root_dir@/test1.txt digest)
+  message("digest: ${digest}")
+]])
+message("")
+
+message([[### TIMESTAMP]])
+scope([[
+  file(TIMESTAMP @root_dir@/test1.txt timestamp)
+  message("timestamp: ${timestamp}")
+  
+  file(TIMESTAMP @root_dir@/test1.txt formatted_timestamp "%Y/%m/%d %H:%M:%S")
+  message("formatted_timestamp: ${formatted_timestamp}")
+  
+  file(TIMESTAMP @root_dir@/test1.txt utc_timestamp UTC)
+  message("utc_timestamp: ${utc_timestamp}")
+]])
+message("")
+
+message([[### RELATIVE_PATH]])
+scope([[
+  get_filename_component(path @root_dir@/../../vendor ABSOLUTE)
+  message("path: ${path}")
+  
+  file(RELATIVE_PATH path ${path} @root_dir@/test1.txt)
+  message("path: ${path}")
+]])
+message("")
+
+message([[### DOWNLOAD]])
+scope([[
+  file(DOWNLOAD http://www.boost.org/ @working_dir@/boost-index.html)
+  execute(ls)
+  
+  file(DOWNLOAD https://www.google.co.jp/ @working_dir@/google-index.html)
+  execute(ls)
+]])
+message("")
+
+message([[## string()]])
+message([[### ASCII]])
+scope([[
+  string(ASCII 40 code)
+  message("code: ${code}")
+]])
+message("")
+
+message([[### FIND]])
+scope([[
+  string(FIND "aaa123a12" "a12" pos)
+  message("pos: ${pos}")
+  
+  string(FIND "aaa123a12" "a12" pos REVERSE)
+  message("pos: ${pos}")
+]])
+message("")
+
+message([[### SUBSTRING]])
+scope([[
+  string(SUBSTRING "aaa123a12" 2 3 sub)
+  message("sub: ${sub}")
+]])
+message("")
+
+message([[### RANDOM]])
+scope([[
+  string(RANDOM result)
+  message("result: ${result}")
+]])
+message("")
+
+message([[## get_filename_component()]])
+scope([[
+  get_filename_component(result "/path/to/script.cmake" DIRECTORY)
+  message("result: ${result}")
+  
+  get_filename_component(result "/path/to/script.cmake" NAME)
+  message("result: ${result}")
+  
+  get_filename_component(result "/path/to/script.cmake" EXT)
+  message("result: ${result}")
+  
+  get_filename_component(result "/path/to/script.cmake" NAME_WE)
+  message("result: ${result}")
+  
+  get_filename_component(result "path/to/script.cmake" ABSOLUTE)
+  message("result: ${result}")
+  
+  get_filename_component(result "/usr/local/include/boost" REALPATH)
+  message("result: ${result}")
+]])
+message("")
+
 message([[## math()]])
-
-function(verify_math expr)
-  math(EXPR result "${expr}")
-  message("${expr} = ${result}")
-endfunction()
-
-verify_math("1 + 1")
-verify_math("1 - 5")
-verify_math("2 * 3")
-verify_math("2 * 3 + (2 + 4)")
-verify_math("1 << 3")
-verify_math("8 >> 2")
-verify_math("12 % 10")
-verify_math("8 | 15")
-verify_math("8 & 15")
+scope([[
+  function(verify_math expr)
+    math(EXPR result "${expr}")
+    message("${expr} = ${result}")
+  endfunction()
+  
+  verify_math("1 + 1")
+  verify_math("1 - 5")
+  verify_math("2 * 3")
+  verify_math("2 * 3 + (2 + 4)")
+  verify_math("1 << 3")
+  verify_math("8 >> 2")
+  verify_math("12 % 10")
+  verify_math("8 | 15")
+  verify_math("8 & 15")
+]])
+message("")
 
 message([[## cmake_host_system_information()]])
-
-cmake_host_system_information(RESULT cores QUERY NUMBER_OF_LOGICAL_CORES)
-
-message("cores: ${cores}")
+scope([[
+  cmake_host_system_information(RESULT cores QUERY NUMBER_OF_LOGICAL_CORES)
+  message("cores: ${cores}")
+  
+  cmake_host_system_information(RESULT info QUERY HOSTNAME FQDN)
+  message("info: ${info}")
+]])
